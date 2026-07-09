4474d4ba3d9a7728cafb30e35856b70cea8d278a
tacchy <ttacchy@gmail.com>
2026-07-08 21:47:12 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,21 +1,32 @@
 let escuro = false;
 
-function tema(){
+
+function tema() {
 
     escuro = !escuro;
 
-    document.getElementById("body").classList.toggle("dark");
 
-    let botao = document.querySelector("button.btn-light");
+    document.getElementById("body")
+        .classList.toggle("dark");
 
-    if(escuro){
 
-        botao.innerHTML="☀️";
+    let imagem = document.getElementById("iconeTema");
 
-    }else{
 
-        botao.innerHTML="🌙";
+    if (escuro) {
+
+        imagem.src = "imagens/slime-noite.png";
+
+    } else {
+
+        imagem.src = "imagens/slime-dia (1).png";
 
     }
 
 }
+
+imagem.classList.add("pular");
+
+setTimeout(() => {
+    imagem.classList.remove("pular");
+}, 500);
